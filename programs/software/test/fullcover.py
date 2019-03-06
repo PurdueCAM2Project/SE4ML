bb2a52a3b195072cbc9c34e2bf8a49375c1f5973
yunglu <luyunghsiang@gmail.com>
2019-03-06 18:59:27 -0500

    add an example of 100% coverage

---
+++ programs/software/test/fullcover.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def f(x, y):
+  if (x > 1):
+    print ('x > 1')
+  else:
+    print ('not x > 1')
+  if (y > 1):
+    print ('y > 1')
+  else:
+    print ('not y > 1')
+  
+if __name__== "__main__":
+  f(2, 2)
+  f(-2, -2)
+
